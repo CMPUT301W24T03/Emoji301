c2e11a9639e8776c22c9495e3ead98e13221125b
AivanD <dolor@ualberta.ca>
2024-03-04 23:23:03 -0700

    - Removed Fragment in AndroidManifest.xml (this was raised by CI hence "x" beside your branch - Created a template for Database.java - updated build.gradle.kts to include missing firestore implementation

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -36,14 +36,18 @@ android {
 }
 
 dependencies {
-
-    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // firebase stuff
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-firestore")
+
+
     implementation("pub.devrel:easypermissions:3.0.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
