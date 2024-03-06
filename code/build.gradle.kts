51e3959a8bc406f38ef488d63f6c995418761c4c
AivanD <dolor@ualberta.ca>
2024-03-05 22:04:45 -0700

    - Created 3 fragments: NameScreenFragment.java, UploadImageScreenFragment.java and PreviewScreenFragment.java - Created the login_nav_graph.xml - modified gradle to navigation
    - missing logic for going from activity_main.xml to fragment_name_screen.xml and so on

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -3,3 +3,15 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
+buildscript {
+//    extra["kotlin_version"] = "1.4.10"
+//    val kotlin_version = "1.4.10"
+    repositories {
+        google()
+    }
+    dependencies {
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
