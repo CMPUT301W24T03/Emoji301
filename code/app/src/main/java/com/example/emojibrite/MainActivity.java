2ec40d9d02cf557c68c30e9da5698ea8b1a0f0e0
Samuel <schan5@ualberta.ca>
2024-03-08 13:45:03 -0700

    mb

--- code/app/src/main/java/com/example/emojibrite/MainActivity.java
+++ code/app/src/main/java/com/example/emojibrite/MainActivity.java
@@ -44,12 +44,12 @@ public class MainActivity extends AppCompatActivity {
 
                 // if the user exists in database, go to the event page
                 Log.d(TAG, "Enter button clicked"); // for debugging
-                Intent intent = new Intent(MainActivity.this, EventHome.class);
-                startActivity(intent);
+                //Intent intent = new Intent(MainActivity.this, EventHome.class);
+                //startActivity(intent);
                 // else go to nameScreenFragment
 
-                //Intent intent = new Intent(MainActivity.this, AccountCreationActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, AccountCreationActivity.class);
+                startActivity(intent);
 //                // if the user exists in database, go to the event page
 //                Log.d(TAG, "Enter button clicked"); // for debugging
 //                Intent intent = new Intent(MainActivity.this, EventHome.class);
