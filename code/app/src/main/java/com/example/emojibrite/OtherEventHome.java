34797bc886b7151d91a008b7736749b836e57649
A.K.E <edirisin@ualberta.ca>
2024-04-05 13:08:43 -0600

    fixes bugs

--- code/app/src/main/java/com/example/emojibrite/OtherEventHome.java
+++ code/app/src/main/java/com/example/emojibrite/OtherEventHome.java
@@ -94,11 +94,11 @@ public class OtherEventHome extends AppCompatActivity {
             public void onClick(View v) {
                 // checking if the user is logged in (as in there is an account associated with the device)
 
-                Bundle bundle = new Bundle();
+
                 // we put in the user ID and geolocation enabled bool
-                bundle.putStringArray("USER", new String[]{user.getProfileUid(), Boolean.toString(user.getEnableGeolocation()), "event"});
+
                 Intent intent = new Intent(OtherEventHome.this, QRScanningActivity.class);
-                intent.putExtras(bundle);
+                intent.putExtra("userObject", user);
                 startActivity(intent);
 
             }
