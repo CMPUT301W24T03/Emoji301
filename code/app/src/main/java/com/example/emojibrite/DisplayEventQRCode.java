dd6dbbd2a2e0b0ea24fa832320c0dca7c8131353
A.K.E <edirisin@ualberta.ca>
2024-04-08 13:30:50 -0600

    java doc fixes bro

--- code/app/src/main/java/com/example/emojibrite/DisplayEventQRCode.java
+++ code/app/src/main/java/com/example/emojibrite/DisplayEventQRCode.java
@@ -19,8 +19,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.example.emojibrite.Database;
-import com.example.emojibrite.R;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
